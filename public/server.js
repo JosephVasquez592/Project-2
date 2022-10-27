8ae3d3be4bdd5325b1473301f629bb74a8464bfc
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2022-10-26 21:09:25 -0600

    installed express.js & package.json

--- public/server.js
+++ public/server.js
@@ -6,15 +6,15 @@ const PORT = 3001;
 
 app.use(express.static("public"));
 
-// app.get("/", (req, res) => res.send("Navigate to /send or /routes"));
+app.get("/", (req, res) => res.send("Navigate to /send or /routes"));
 
-// app.get("/send", (req, res) =>
-//   res.sendFile(path.join(__dirname, "public/send.html"))
-// );
+app.get("/send", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/send.html"))
+);
 
-// app.get("/paths", (req, res) =>
-//   res.sendFile(path.join(__dirname, "public/paths.html"))
-// );
+app.get("/paths", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/paths.html"))
+);
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
