ad113661328c31013d81d4d1c8d16fcf61c275a3
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2022-10-27 18:53:13 -0600

    added handlebars engine, corrected PORT

--- public/server.js
+++ public/server.js
@@ -2,20 +2,22 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
-app.use(express.static("public"));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => res.send("Navigate to /send or /routes"));
+// app.get("/", (req, res) => res.send("Navigate to /send or /routes"));
 
-app.get("/send", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/send.html"))
-);
+// app.get("/send", (req, res) =>
+//   res.sendFile(path.join(__dirname, "public/send.html"))
+// );
 
-app.get("/paths", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/paths.html"))
-);
+// app.get("/paths", (req, res) =>
+//   res.sendFile(path.join(__dirname, "public/paths.html"))
+// );
 
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
