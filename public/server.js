dc1c33f77eab8bc5756f6698ccec34f97c854d02
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2022-10-27 19:29:12 -0600

    added auth & messages route

--- public/server.js
+++ public/server.js
@@ -8,6 +8,12 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 app.use(express.static(path.join(__dirname, "public")));
 
+const auth = require("./routes/auth");
+const messages = require("./routes/messages");
+
+app.use("/api/auth", auth);
+app.use("/api/messages", messages);
+
 // app.get("/", (req, res) => res.send("Navigate to /send or /routes"));
 
 // app.get("/send", (req, res) =>
