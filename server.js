705ab0963cb926e0a447bc6411dbcd3ae41e468e
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2022-10-30 20:57:41 -0600

    moved server file and others outside public folder

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ app.use(express.static(path.join(__dirname, "public")));
 const auth = require("./routes/auth");
 const messages = require("./routes/messages");
 
+app.get("/", (req, res) => {
+  res.send("Visit http://localhost:3001/api");
+});
+
 app.use("/api/auth", auth);
 app.use("/api/messages", messages);
-
-// app.get("/", (req, res) => res.send("Navigate to /send or /routes"));
 
 // app.get("/send", (req, res) =>
 //   res.sendFile(path.join(__dirname, "public/send.html"))
@@ -24,6 +26,6 @@ app.use("/api/messages", messages);
 //   res.sendFile(path.join(__dirname, "public/paths.html"))
 // );
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
+app.listen(PORT, () =>
+  console.log(`Server listening at http://localhost:${PORT}`)
+);
